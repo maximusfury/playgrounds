0c6f878243700503b699cae6d88a20830824fe41
Nicholas Monterroza <nickmonterroza916@gmail.com>
2017-12-27 14:39:36 -0600

    number, string

---
+++ my-first-playground.playground/Pages/numbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let constant = 5.0/2
+let remainer = 3%4
+type(of: constant)
+
+
+//: [Next](@next)
+
+let multiplyfloat = constant * 2
+
---
+++ my-first-playground.playground/Pages/string.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+let letter = "Hello,"
+type(of: letter)
+
+let letter2 = "world!"
+
+var string = "Hello, world!"
+type(of:string)
+string.count
+
+let other = letter + " " + letter2
+
